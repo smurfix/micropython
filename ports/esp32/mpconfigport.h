c6f5ad13e1904499df48b9100143ec075f07a828
Matthias Urlichs <matthias@urlichs.de>
2021-05-27 12:18:17 +0200

    Allow overriding by the board config

--- ports/esp32/mpconfigport.h
+++ ports/esp32/mpconfigport.h
@@ -174,8 +174,13 @@
 #define MICROPY_FATFS_RPATH                 (2)
 #define MICROPY_FATFS_MAX_SS                (4096)
 #define MICROPY_FATFS_LFN_CODE_PAGE         437 /* 1=SFN/ANSI 437=LFN/U.S.(OEM) */
+
+#ifndef mp_type_fileio
 #define mp_type_fileio                      mp_type_vfs_fat_fileio
+#endif
+#ifndef mp_type_textio
 #define mp_type_textio                      mp_type_vfs_fat_textio
+#endif
 
 // use vfs's functions for import stat and builtin open
 #define mp_import_stat mp_vfs_import_stat
