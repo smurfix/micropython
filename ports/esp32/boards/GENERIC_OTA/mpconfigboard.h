7684dd8fb87fdbd2f53f5f87658656cbe84319ed
Matthias Urlichs <matthias@urlichs.de>
2021-05-27 12:20:12 +0200

    OTA: disable some more stuff

--- ports/esp32/boards/GENERIC_OTA/mpconfigboard.h
+++ ports/esp32/boards/GENERIC_OTA/mpconfigboard.h
@@ -1,2 +1,11 @@
 #define MICROPY_HW_BOARD_NAME "4MB/OTA module"
 #define MICROPY_HW_MCU_NAME "ESP32"
+
+#undef MICROPY_VFS_FAT
+#define MICROPY_VFS_FAT (0)
+
+#undef mp_type_fileio
+#define mp_type_fileio                      mp_type_vfs_lfs2_fileio
+#undef mp_type_textio
+#define mp_type_textio                      mp_type_vfs_lfs2_textio
+
