284c2ee0b38a72b0387623663f73aeda54dce3b5
Matthias Urlichs <matthias@urlichs.de>
2023-09-03 19:08:52 +0200

    extmod/asyncio/stream: Handle closing a stream.
    CPython's `stream.close` method is synchronous, which is unfortunate.
    
    Protect against leaving a stream open by mistake by closing the
    underlying socket in all of `close`, `wait_closed` and `__aexit__`.
    
    Signed-off-by: Matthias Urlichs <matthias@urlichs.de>

--- extmod/asyncio/stream.py
+++ extmod/asyncio/stream.py
@@ -5,6 +5,7 @@ from . import core
 
 
 class Stream:
+    # The underlying socket must have an idempotent `close` method.
     def __init__(self, s, e={}):
         self.s = s
         self.e = e
@@ -13,11 +14,20 @@ class Stream:
     def get_extra_info(self, v):
         return self.e[v]
 
-    def close(self):
+    async def __aenter__(self):
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        self.s.close()
         pass
 
+    def close(self):
+        # The (old) CPython idiom is to call `close`, then immediately
+        # follow up with `await stream.wait_closed`.
+        self.s.close()
+
     async def wait_closed(self):
-        # TODO yield?
+        # XXX yield?
         self.s.close()
 
     # async
