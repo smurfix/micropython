f6c99cba9669ef69953c2acc5a417794a420c381
Matthias Urlichs <matthias@urlichs.de>
2023-10-06 21:06:26 +0200

    MoaT-ify ESP8266

--- ports/esp8266/boards/manifest.py
+++ ports/esp8266/boards/manifest.py
@@ -1,7 +1,9 @@
 freeze("$(PORT_DIR)/modules")
 # require("aioespnow")
 require("bundle-networking")
-require("dht")
-require("ds18x20")
-require("neopixel")
-require("onewire")
+#require("dht")
+#require("ds18x20")
+#require("neopixel")
+#require("onewire")
+
+include("../../../../../moat/micro/_embed/lib")
