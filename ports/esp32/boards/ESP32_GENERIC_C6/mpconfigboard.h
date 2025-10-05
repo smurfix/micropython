a9aefc1deda0cde76ab9d5ab2720dcb109fcc33b
Matthias Urlichs <matthias@urlichs.de>
2025-10-05 22:24:47 +0200

    disable USB+JTAG.

--- ports/esp32/boards/ESP32_GENERIC_C6/mpconfigboard.h
+++ ports/esp32/boards/ESP32_GENERIC_C6/mpconfigboard.h
@@ -7,3 +7,6 @@
 
 // Enable UART REPL for modules that have an external USB-UART and don't use native USB.
 #define MICROPY_HW_ENABLE_UART_REPL         (1)
+
+// turn off USB and JTAG. It's a generic board.
+#define MICROPY_HW_ESP_USB_SERIAL_JTAG      (0)
